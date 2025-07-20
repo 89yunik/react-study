1dc6782820d1c897b1fab24efd11509ce7f0c23c
Joshua, 엄민식 <joshua5451@naver.com>
2025-07-20 21:18:56 +0900

    chore: rebase test

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,17 +1,18 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { FC } from "react"
-import { Movie } from "./Movie"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { FC } from "react";
+import { Movie } from "./Movie";
 
 const App: FC = () => {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
 
   // queryClient.invalidateQueries()
 
   return (
     <QueryClientProvider client={queryClient}>
       <Movie></Movie>
+      <div>123123123123</div>
     </QueryClientProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
