af94778c1a6528781bba724e6be03d1f3f5a53ae
Nick Yoon <89yunik@gmail.com>
2025-07-27 16:18:40 +0900

    fix: favorite data 저장 방식 변경 localstorage -> tmdb api 사용

--- src/App/hooks/useFavorites.ts
+++ src/App/hooks/useFavorites.ts
@@ -1,23 +1,56 @@
 import { useEffect, useState } from "react";
+import { MovieResponse } from "../../types/common";
 
 export const useFavorites = () => {
   const [favorites, setFavorites] = useState<number[]>([]);
 
   useEffect(() => {
-    const stored = JSON.parse(localStorage.getItem("favorites") || "[]");
-    setFavorites(stored);
+    const getFavoritMovies = async () => {
+      const accountId = localStorage.getItem("accountId");
+      if (!accountId) return;
+      const moviesResponse = await fetch(`https://api.themoviedb.org/3/account/${accountId}/favorite/movies`, {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${process.env.REACT_APP_TMDB_AUTHENTICATION_KEY}`,
+          "content-type": "application/json",
+        },
+      });
+
+      const moviesJson: MovieResponse = await moviesResponse.json();
+      const movies = moviesJson.results ? moviesJson.results.map((movie) => movie.id) : [];
+
+      setFavorites(movies);
+    };
+
+    getFavoritMovies();
   }, []);
 
   const isFavorite = (id: number) => favorites.includes(id);
 
   const toggle = (id: number) => {
     const exists = favorites.includes(id);
-    const updated = exists
-      ? favorites.filter((favorite) => favorite !== id)
-      : [...favorites, id];
+    const updated = exists ? favorites.filter((favorite) => favorite !== id) : [...favorites, id];
 
     setFavorites(updated);
-    localStorage.setItem("favorites", JSON.stringify(updated));
+    const updateFavoriteMovies = async (media_id: number, favorite: Boolean) => {
+      const accountId = localStorage.getItem("accountId");
+      if (!accountId) return;
+      await fetch(`https://api.themoviedb.org/3/account/${accountId}/favorite`, {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${process.env.REACT_APP_TMDB_AUTHENTICATION_KEY}`,
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          media_type: "movie",
+          media_id,
+          favorite,
+        }),
+      });
+    };
+    updateFavoriteMovies(id, !exists);
   };
 
   return { favorites, isFavorite, toggle };
