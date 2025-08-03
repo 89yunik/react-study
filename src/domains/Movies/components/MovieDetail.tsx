f5868b59ff7aca5a53fc29245a2a3fcedf7f4a20
Nick Yoon <89yunik@gmail.com>
2025-08-03 17:52:43 +0900

    feat: 영화 상세 페이지 UI 만들기

--- src/domains/Movies/components/MovieDetail.tsx
+++ src/domains/Movies/components/MovieDetail.tsx
@@ -1,16 +1,22 @@
 import { FC } from "react";
 import { useLocation } from "react-router-dom";
+import styles from "./MovieDetail.module.css";
+import clsx from "clsx";
 
 export const MovieDetail: FC = () => {
   const location = useLocation();
   const movieDetail = location.state;
   return (
-    <div>
-      <img src={`https://media.themoviedb.org/t/p/w154/${movieDetail.poster_path}`} alt={movieDetail.title} />
-      <h2>{movieDetail.title}</h2>
-      <h3>{movieDetail.original_title}</h3>
-      <h3>개요</h3>
-      <p>{movieDetail.overview}</p>
+    <div className={clsx(styles.movie_detail)}>
+      <div className={clsx(styles.poster_wrapper)}>
+        <img className={clsx(styles.poster)} src={`https://media.themoviedb.org/t/p/w300_and_h450_bestv2/${movieDetail.poster_path}`} alt={movieDetail.title} />
+      </div>
+      <div className={clsx(styles.movie_texts)}>
+        <h2 className={clsx(styles.title)}>{movieDetail.title}</h2>
+        <h3>{movieDetail.original_title}</h3>
+        <h3>개요</h3>
+        <p className={clsx(styles.overview)}>{movieDetail.overview}</p>
+      </div>
     </div>
   );
 };
